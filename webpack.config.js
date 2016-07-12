55a1262911015f146c9e248d3eedd47235d8c227
kiranps <pskirann@gmail.com>
2016-07-12 13:30:19 +0530

    added config for styles,fonts,images for webpack dev

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'eval',
   entry: [
-    'webpack-dev-server/client?http://localhost:9001',
+    'webpack-dev-server/client?http://0.0.0.0:9001',
     'webpack/hot/only-dev-server',
     './src/index'
   ],
@@ -21,6 +21,18 @@ module.exports = {
       test: /\.js$/,
       loaders: ['react-hot', 'babel'],
       include: path.join(__dirname, 'src')
+    },
+    {
+      test: /\.(png|jpg|svg)(\?v=\d+\.\d+\.\d+)?/,
+      loader: ['url-loader?limit=100000&mimetype=image/png&name=images/[name].[ext]', 'react-hot']
+    },
+    {
+      test: /\.(eot|svg|otf|ttf|woff|woff2)(\?v=\d+\.\d+\.\d+)?/,
+      loader: ['file?name=fonts/[name].[ext]', 'react-hot']
+    },
+    {
+      test: /\.css$/,
+      loaders: ['react-hot', 'style', 'css'],
     }]
   }
 };
